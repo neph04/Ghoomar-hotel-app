b777cc1c8e665baa78466be16be02e861293071f
neph04 <sidyy8473@gmail.com>
2021-10-29 14:32:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/layouts/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        getSupportActionBar().hide();
+
+        login=findViewById(R.id.buttonlogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login has been clicked", Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(MainActivity.this,signin_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/layouts/signin_activity.java
@@ -0,0 +1,15 @@
+package com.example.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signin_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+    }
+}
